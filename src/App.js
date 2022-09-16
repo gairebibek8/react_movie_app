2fb9df6653f625433bd90379b3c67f93c562950f
Bibek Gaire <gairebibek8@gmail.com>
2022-09-16 16:09:44 -0500

    Movies list pops from API call when searched

--- src/App.js
+++ src/App.js
@@ -1,35 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import MovieList from "./components/MovieList";
+import MovieListHeading from "./components/MovieListHeading";
+import SearchBox from "./components/SearchBox";
 
 const App = () => {
-  const [movies, setMovies] = useState([
-    {
-      "Title": "Star Wars",
-      "Year": "1977",
-      "imdbID": "tt0076759",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
-  },
-  {
-      "Title": "Star Wars: Episode V - The Empire Strikes Back",
-      "Year": "1980",
-      "imdbID": "tt0080684",
-      "Type": "movie",
-      "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
-  },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const getMovieRequest = async(searchValue) => {
+    const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=b73c116c`
+
+    const response = await fetch (url);
+    const responseJson = await response.json();
+
+    if(responseJson.Search){
+      setMovies(responseJson.Search);
+    }
+
+    // console.log(responseJson);
+    // setMovies(responseJson.Search);
+
+  };
+
+  useEffect (() => {
+    getMovieRequest(searchValue);
+  }, [searchValue]);
 
 
-  return <div>
-    <MovieList movies = {movies}/>
+  return (
+    <div className="container_fluid movie_app">
+      <div className="row d-flex align-items-center mt-4 mb-4">
+          <MovieListHeading  heading= "Movies" />
+          <SearchBox searchValue= {searchValue}  setSearchValue= {setSearchValue} />
+      </div>
+
+      <div className="row">
+      <MovieList movies = {movies} />
+      </div>
+
     </div>
-
+  );
 };
 
 
 
 
-  
 
 
 export default App;
