b52c08454c0a2070c6a365f2e5a9a1067de593ab
Bibek Gaire <gairebibek8@gmail.com>
2022-09-16 13:33:01 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import MovieList from "./components/MovieList";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [movies, setMovies] = useState([
+    {
+      "Title": "Star Wars",
+      "Year": "1977",
+      "imdbID": "tt0076759",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BNzg4MjQxNTQtZmI5My00YjMwLWJlMjUtMmJlY2U2ZWFlNzY1XkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg"
+  },
+  {
+      "Title": "Star Wars: Episode V - The Empire Strikes Back",
+      "Year": "1980",
+      "imdbID": "tt0080684",
+      "Type": "movie",
+      "Poster": "https://m.media-amazon.com/images/M/MV5BYmU1NDRjNDgtMzhiMi00NjZmLTg5NGItZDNiZjU5NTU4OTE0XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SX300.jpg"
+  },
+  ]);
+
+
+  return <div>
+    <MovieList movies = {movies}/>
     </div>
-  );
-}
+
+};
+
+
+
+
+  
+
 
 export default App;
