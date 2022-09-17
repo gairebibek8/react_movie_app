4a777d31b28f8f8235a7ff28902661f9db6466f7
Bibek Gaire <gairebibek8@gmail.com>
2022-09-16 20:55:14 -0500

    Final changes for scroll

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.css";
+import "./App.css";
 import MovieList from "./components/MovieList";
 import MovieListHeading from "./components/MovieListHeading";
 import SearchBox from "./components/SearchBox";
 
 const App = () => {
   const [movies, setMovies] = useState([]);
-  const [searchValue, setSearchValue] = useState("");
+  const [searchValue, setSearchValue] = useState("avengers");
 
   const getMovieRequest = async(searchValue) => {
     const url = `https://www.omdbapi.com/?s=${searchValue}&apikey=b73c116c`
@@ -30,7 +30,7 @@ const App = () => {
 
 
   return (
-    <div className="container_fluid movie_app">
+    <div className="container-fluid movie_app">
       <div className="row d-flex align-items-center mt-4 mb-4">
           <MovieListHeading  heading= "Movies" />
           <SearchBox searchValue= {searchValue}  setSearchValue= {setSearchValue} />
